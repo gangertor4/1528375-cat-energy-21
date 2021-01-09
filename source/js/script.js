969fa891f0d8323115f401e86b89a425c368ec90
gangertor4 <tor4911@yandex.ru>
2021-01-09 18:17:21 +0300

    Правки 1

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,8 @@
 var navToggle = document.querySelector('.page-header__nav-toggle');
 var navMain = document.querySelector('.main-nav');
 
+navMain.classList.remove('main-nav--nojs');
+
 navToggle.addEventListener('click', function() {
     if (navToggle.classList.contains('page-header__nav-toggle--burger')) {
         navToggle.classList.remove('page-header__nav-toggle--burger');
@@ -15,23 +17,23 @@ navToggle.addEventListener('click', function() {
     }
 });
 
-var catImageFat = document.querySelector('.example__image--before');
-var catImageSkinny = document.querySelector('.example__image--after');
-var beforeBtn = document.querySelector('.slider__description--before');
-var afterBtn = document.querySelector('.slider__description--after');
-var sliderBar = document.querySelector('.slider__bar');
+// var catImageFat = document.querySelector('.example__image--before');
+// var catImageSkinny = document.querySelector('.example__image--after');
+// var beforeBtn = document.querySelector('.slider__description--before');
+// var afterBtn = document.querySelector('.slider__description--after');
+// var sliderBar = document.querySelector('.slider__bar');
 
-beforeBtn.addEventListener('click', function() {
-    catImageFat.classList.remove('example__image--hidden');
-    catImageSkinny.classList.add('example__image--hidden');
-    sliderBar.classList.remove('slider__bar--after');
-    sliderBar.classList.add('slider__bar--before');
-});
+// beforeBtn.addEventListener('click', function() {
+//     catImageFat.classList.remove('example__image--hidden');
+//     catImageSkinny.classList.add('example__image--hidden');
+//     sliderBar.classList.remove('slider__bar--after');
+//     sliderBar.classList.add('slider__bar--before');
+// });
 
-afterBtn.addEventListener('click', function() {
-    catImageSkinny.classList.remove('example__image--hidden');
-    catImageFat.classList.add('example__image--hidden');
-    sliderBar.classList.remove('slider__bar--before');
-    sliderBar.classList.add('slider__bar--after');
-});
+// afterBtn.addEventListener('click', function() {
+//     catImageSkinny.classList.remove('example__image--hidden');
+//     catImageFat.classList.add('example__image--hidden');
+//     sliderBar.classList.remove('slider__bar--before');
+//     sliderBar.classList.add('slider__bar--after');
+// });
 
