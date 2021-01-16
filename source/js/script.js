d5ba1eca0689c107ff6a5b304bdb2b785f470e04
gangertor4 <tor4911@yandex.ru>
2021-01-16 17:29:45 +0300

    Фон в блоке example 4

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ var navToggle = document.querySelector('.page-header__nav-toggle');
 var navMain = document.querySelector('.main-nav');
 
 navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('page-header__nav-toggle--cross');
+navToggle.classList.add('page-header__nav-toggle--burger');
 
 navToggle.addEventListener('click', function() {
     if (navToggle.classList.contains('page-header__nav-toggle--burger')) {
