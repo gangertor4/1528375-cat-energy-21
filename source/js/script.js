36b69aee9bc02222ee37508a81dea456ca13938b
gangertor4 <tor4911@yandex.ru>
2021-04-17 14:43:02 +0300

    Слайдер с котиками

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
-var navToggle = document.querySelector('.page-header__nav-toggle');
-var navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.page-header__nav-toggle');
+const navMain = document.querySelector('.main-nav');
 
 navMain.classList.remove('main-nav--nojs');
 navToggle.classList.remove('page-header__nav-toggle--nojs');
@@ -18,3 +18,26 @@ navToggle.addEventListener('click', function() {
         navMain.classList.add('main-nav--closed');
     }
 });
+
+const beforeBtn = document.querySelector('.slider__description--before');
+const afterBtn = document.querySelector('.slider__description--after');
+const beforeCat = document.querySelector('.example__image--before');
+const afterCat = document.querySelector('.example__image--after');
+const beforeBar = document.querySelector('.slider__bar');
+
+afterBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    console.log('SHIT');
+    beforeCat.classList.add('example__image--hidden');
+    afterCat.classList.remove('example__image--hidden');
+    beforeBar.classList.remove('slider__bar--before');
+    beforeBar.classList.add('slider__bar--after');
+})
+
+beforeBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    afterCat.classList.add('example__image--hidden');
+    beforeCat.classList.remove('example__image--hidden');
+    beforeBar.classList.remove('slider__bar--after');
+    beforeBar.classList.add('slider__bar--before');
+})
